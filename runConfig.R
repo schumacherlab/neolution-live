65833e1255c13ad75c3c58baceedb4a8482eb7d3
Lorenzo Fanchi <l.fanchi@nki.nl>
2015-09-04 16:56:50 +0200

    add runConfig.R
    moved lookup parameters and user-defined settings to separate script
    
    
    Former-commit-id: b896250a3dbaaa304d5c31593445bd4a5ff762ff

---
+++ runConfig.R
@@ -0,0 +1,16 @@
+#### Lookup parameters and other configuration options can be set here ####
+
+# get number of cores available for parallel processing
+numberOfWorkers=5
+
+# set min. affinity (in nM), min. chop score & min. RNA expression
+affinityLimit=500
+chopLimit=0.5
+expressionLimit=0
+
+# set peptide length to be analysed (only 9mers supported at the moment)
+xMer=9
+
+# set hla types to be analysed and location of file containing RNA expression data
+hlaTypes=c("A0101","A0201","A0301","B0702","B0801")
+rnaExpressionFile="20150723_RNAexpression_likelihood_TCGA_IlluminaHiSeq.csv"
