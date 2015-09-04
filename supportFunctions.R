210025de8b36a338ab299a17dd19bcd5f56b2f0c
Lorenzo Fanchi <l.fanchi@nki.nl>
2015-09-04 17:47:11 +0200

    added funtions
    - checkPredictors.. from predictionLogic.R
    - checkTissue.. from queryDatabase.R
    
    
    Former-commit-id: 6bf7db954bef803c4f7c756479ce29be92b75312

--- supportFunctions.R
+++ supportFunctions.R
@@ -16,3 +16,27 @@ multiMixedOrder=function(..., na.last = TRUE, decreasing = FALSE){
     list(na.last = na.last, decreasing = decreasing)
   ))
 }
+
+# check for presence of necessary tools
+checkPredictorPaths=function(){
+  predictors_list=c(netMHCpanpath,netChoppath)
+  print("Checking for presence of predictors...")
+  for(i in 1:length(predictors_list)){
+    if (!file.exists(predictors_list[i])){
+      stop(paste("Missing: ",predictors_list[i],", stopping script execution",sep=""))
+    } else {
+      print(paste("Present: ",predictors_list[i],", continuing...",sep=""))
+    }
+  }
+}
+
+# check if rna expression data is available for input tissue type
+checkTissueTypeInput=function(inputtissue,availabletissues){
+  if (any(inputtissue==availabletissues)){
+    #message(paste("Input tissuetype: ",tissueTypeInput,sep=""))
+  } else{
+    #message(paste("Input tissue type (",tissueTypeInput,") has no available RNAseq data",sep=""))
+    #message(paste("Available tissues: ", paste(availableTissueTypes,collapse=","),sep=""))
+    stop("Input tissue type not supported")
+  }
+}
