878f75e96ed94c7658770030581355b34584d4b5
Lorenzo Fanchi <l.fanchi@nki.nl>
2015-09-10 15:07:53 +0200

    reworked vars to be more flexible & intuitive
    Former-commit-id: 5501462b0ba2a8d00922dfa2c50747395e6efe0f

--- runConfig.R
+++ runConfig.R
@@ -1,6 +1,6 @@
 #### Lookup parameters and other configuration options can be set here ####
 
-# get number of cores available for parallel processing
+# set number of cores available for parallel processing
 numberOfWorkers=5
 
 # set min. affinity (in nM), min. chop score & min. RNA expression
@@ -9,16 +9,26 @@ chopLimit=0.5
 expressionLimit=0
 
 # set peptide length to be analysed (only 9mers supported at the moment)
-xMer=9
+peptideLength=9
 
-# set hla types to be analysed and location of file containing RNA expression data
-hlaTypes=c("A0101","A0201","A0301","B0702","B0801")
-rnaExpressionFile="20150723_RNAexpression_likelihood_TCGA_IlluminaHiSeq.csv"
+# set hla types to be analysed 
+hlaTypes=c("A0101",
+           "A0201",
+           "A0301",
+           "B0702",
+           "B0801")
+
+# set location of file containing RNA expression data
+rnaExpressionPath="./rna_expr_data/20150723_RNAexpression_likelihood_TCGA_IlluminaHiSeq.csv"
+
+# set location and base filename of self-peptide lists
+# self-peptide list filenames should follow convention: 'selfPeptideListBaseName'_'hlaTypes[x]'_'peptideLength'mer_epitopes.csv
+selfPeptideListPath="./selflists_v2.4"
+selfPeptideListBaseName="20140610_human_proteome"
 
 # set predictor paths
-netMHCpath="/home/NKI/l.fanchi/netMHC-3.0/netMHC-3.0"
-netMHCpanpath="/home/NKI/l.fanchi/netMHCpan-2.4/netMHCpan"
-netChoppath="/home/NKI/l.fanchi/netchop-3.1/bin/netChop"
+predictorPaths=data.table(netMHCpan="/home/NKI/l.fanchi/netMHCpan-2.4/netMHCpan",
+                          netChop="/home/NKI/l.fanchi/netchop-3.1/bin/netChop")
 
 # set mysql configuration
 sqlhost = "medoid"
