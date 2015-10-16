f1c4321124d578b4a91dc98c337e7ba59a1c2e67
Lorenzo Fanchi <l.fanchi@nki.nl>
2015-10-16 18:52:05 +0200

    clarified column names
    Former-commit-id: e779a2dcdaa78d0ec07637ea0ed560d009a9ddd9

--- support_scripts/peptideConstructionLogic.R
+++ support_scripts/peptideConstructionLogic.R
@@ -6,23 +6,23 @@ buildPeptideList=function(variant,peptidelength){
   # make peptides
   if (n_normal>0){
     normal_peptide=sapply(seq(1,n_normal,1),function(i) substr(x = variant$peptidecontextnormal,start = i, stop = i+(peptidelength-1)))
-    c_term_pos=seq(peptidelength,nchar(variant$peptidecontextnormal),1)
-    normal=unique(data.table(normal_peptide,c_term_pos), by = "normal_peptide")
+    normal_c_term_pos=seq(peptidelength,nchar(variant$peptidecontextnormal),1)
+    normal=unique(data.table(normal_peptide,normal_c_term_pos), by = "normal_peptide")
   } else{
     stop()
   }
   
   if (n_tumor>0){
     tumor_peptide=sapply(seq(1,n_tumor,1),function(i) substr(x = variant$peptidecontexttumor,start = i, stop = i+(peptidelength-1)))
-    c_term_pos=seq(peptidelength,nchar(variant$peptidecontexttumor),1)
-    tumor=unique(x = data.table(tumor_peptide,c_term_pos), by = "tumor_peptide")
+    tumor_c_term_pos=seq(peptidelength,nchar(variant$peptidecontexttumor),1)
+    tumor=unique(x = data.table(tumor_peptide,tumor_c_term_pos), by = "tumor_peptide")
   } else{
     stop()
   }
   
   # select tumor peptides != normal peptides; select corresponding normal peptides
   tumor=tumor[tumor$tumor_peptide %ni% normal$normal_peptide]
-  normal=normal[match(tumor$c_term_pos,normal$c_term_pos,nomatch = FALSE)]
+  normal=normal[match(tumor$tumor_c_term_pos,normal$normal_c_term_pos,nomatch = FALSE)]
   
   return(list(normal,tumor))
 }
