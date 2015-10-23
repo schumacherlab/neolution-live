6a70bd0ddc14b0e582ae9bdcddb371d70bbd9dc1
Lorenzo Fanchi <l.fanchi@nki.nl>
2015-10-23 12:24:42 +0200

    code styling
    Former-commit-id: e295797ae21afa8fde4b88ed5fdf0cffa274b27c

--- support_scripts/parseLogic.R
+++ support_scripts/parseLogic.R
@@ -1,4 +1,4 @@
-readFastaFile=function(file){
+readFastaFile = function(file) {
   # read data
   lines = readLines(file)
   
@@ -7,7 +7,7 @@ readFastaFile=function(file){
   
   # find how many sequences in data
   nseq = length(ind)
-  if(nseq == 0){
+  if(nseq == 0) {
     stop("no line starting with a > character found")
   }
   
@@ -20,45 +20,46 @@ readFastaFile=function(file){
   sequences = sapply(seq_len(nseq), function(i) paste(lines[start[i]:end[i]], collapse = ""))
   
   # get names of sequences
-  seqnames = sapply(seq_len(nseq), function(i) gsub(pattern = "^>",replacement = "",x = lines[ind[i]]))
+  seqnames = sapply(seq_len(nseq), function(i) gsub(pattern = "^>", replacement = "", x = lines[ind[i]]))
   
   # put in a table
-  table=data.table(sequence_id=seqnames,sequence=sequences)
+  table = data.table(sequence_id = seqnames, sequence = sequences)
   
   return(table)
 }
 
-returnProcessedVariants=function(id,variants){
-  variants$variant_id=paste(id,1:nrow(variants),sep = "-")
+returnProcessedVariants = function(id, variants) {
+  variants$variant_id = paste(id, 1:nrow(variants), sep = "-")
   
   # rename column headers in case their differ from NKI GCF columns
-  if(all(c("Gene","transcriptid","Cufflinks FPKM value (expression level)") %in% names(variants))){
+  if(all(c("Gene", "transcriptid", "Cufflinks FPKM value (expression level)") %in% names(variants))) {
     setnames(x = variants,
-             old = c("Gene","transcriptid","Cufflinks FPKM value (expression level)"),
-             new = c("symbol","transcript","gene_FPKM"))
+             old = c("Gene", "transcriptid", "Cufflinks FPKM value (expression level)"),
+             new = c("symbol", "transcript", "gene_FPKM"))
   }
   
   # take subset of columns
-  variantssubset=unique(x = subset(x = variants,
-                                   select = c("variant_id","symbol","transcript","peptidecontextnormal","peptidecontexttumor","gene_FPKM")),
-                        by = c("peptidecontextnormal","peptidecontexttumor"))
+  variantssubset = unique(x = subset(x = variants,
+                                     select = c("variant_id", "symbol", "transcript", "peptidecontextnormal", "peptidecontexttumor", "gene_FPKM")),
+                          by = c("peptidecontextnormal", "peptidecontexttumor"))
   
   # remove any amino acid sequence after stop codon(s)
-  variantssubset$peptidecontextnormal=gsub(pattern = "\\*[A-Z*]*",
-                                           replacement = "",
-                                           x = variantssubset$peptidecontextnormal)
+  variantssubset$peptidecontextnormal = gsub(pattern = "\\*[A-Z*]*",
+                                             replacement = "",
+                                             x = variantssubset$peptidecontextnormal)
   
-  variantssubset$peptidecontexttumor=gsub(pattern = "\\*[A-Z*]*",
-                                          replacement = "",
-                                          x = variantssubset$peptidecontexttumor)
+  variantssubset$peptidecontexttumor = gsub(pattern = "\\*[A-Z*]*",
+                                            replacement = "",
+                                            x = variantssubset$peptidecontexttumor)
   
   # take unique subset where normal context != tumor context
-  variantssubset=unique(x = subset(x = variantssubset,
-                                   subset = !variantssubset$peptidecontextnormal==variantssubset$peptidecontexttumor),
-                        by = c("peptidecontextnormal","peptidecontexttumor"))
+  variantssubset = unique(x = subset(x = variantssubset,
+                                     subset = !variantssubset$peptidecontextnormal == variantssubset$peptidecontexttumor),
+                          by = c("peptidecontextnormal", "peptidecontexttumor"))
   
   # if alpha characters are found in cufflinks data, set to 0 (means failed or low data)
-  variantssubset$gene_FPKM[grepl(pattern = "[A-Za-z]",x = variantssubset$gene_FPKM)]=0
+  variantssubset$gene_FPKM[grepl(pattern = "[A-Za-z]",
+                                 x = variantssubset$gene_FPKM)] = 0
   
   return(variantssubset)
 }
