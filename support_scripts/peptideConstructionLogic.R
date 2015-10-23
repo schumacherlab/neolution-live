1b0025473ccbd487c17605768d7c3e144de0f496
Lorenzo Fanchi <l.fanchi@nki.nl>
2015-10-23 12:24:52 +0200

    code styling
    Former-commit-id: 2136a34aa3f301314bd935296c177df261faaf79

--- support_scripts/peptideConstructionLogic.R
+++ support_scripts/peptideConstructionLogic.R
@@ -1,52 +1,67 @@
-buildPeptideList=function(sequences,peptidelength){
-  if (doSingleSequencePrediction){
+buildPeptideList = function(sequences, peptidelength) {
+  if (doSingleSequencePrediction) {
     # determine how many peptides can be made
-    n_seq=nchar(sequences$sequence)-(peptidelength-1)
+    n_seq = nchar(sequences$sequence) - (peptidelength - 1)
     
     # make peptides
-    if (n_seq>0){
-      peptide=sapply(seq(1,n_seq,1),function(i) substr(x = sequences$sequence,start = i, stop = i+(peptidelength-1)))
-      c_term_pos=seq(peptidelength,nchar(sequences$sequence),1)
-      table=unique(data.table(peptide,c_term_pos), by = "peptide")
-      table[,sequence_id := sequences$sequence_id]
-    } else{
-      table=data.table()
+    if (n_seq > 0) {
+      peptide = sapply(seq(from = 1, to = n_seq, by = 1), function(i) substr(x = sequences$sequence,
+                                                                             start = i,
+                                                                             stop = i + (peptidelength - 1)))
+      c_term_pos = seq(from = peptidelength,
+                       to = nchar(sequences$sequence),
+                       by = 1)
+      table = unique(x = data.table(peptide, c_term_pos),
+                     by = "peptide")
+      table[, sequence_id := sequences$sequence_id]
+    } else {
+      table = data.table()
     }
     
     return(table)
     
   } else {
     # determine how many peptides can be made
-    n_normal=nchar(sequences$peptidecontextnormal)-(peptidelength-1)
-    n_tumor=nchar(sequences$peptidecontexttumor)-(peptidelength-1)
+    n_normal = nchar(sequences$peptidecontextnormal) - (peptidelength - 1)
+    n_tumor = nchar(sequences$peptidecontexttumor) - (peptidelength - 1)
     
     # make peptides
-    if (n_normal>0){
-      peptide=sapply(seq(1,n_normal,1),function(i) substr(x = sequences$peptidecontextnormal,start = i, stop = i+(peptidelength-1)))
-      c_term_pos=seq(peptidelength,nchar(sequences$peptidecontextnormal),1)
-      normal=unique(data.table(peptide,c_term_pos), by = "peptide")
-      normal[,variant_id := sequences$variant_id]
-      normal[,gene_symbol := sequences$symbol]
-      normal[,rna_expression_fpkm := sequences$gene_FPKM]
-    } else{
-      normal=data.table()
+    if (n_normal > 0) {
+      peptide = sapply(seq(from = 1, to = n_normal, by = 1), function(i) substr(x = sequences$peptidecontextnormal,
+                                                                                start = i,
+                                                                                stop = i + (peptidelength - 1)))
+      c_term_pos = seq(from = peptidelength,
+                       to = nchar(sequences$peptidecontextnormal),
+                       by = 1)
+      normal = unique(x = data.table(peptide, c_term_pos),
+                      by = "peptide")
+      normal[, variant_id := sequences$variant_id]
+      normal[, gene_symbol := sequences$symbol]
+      normal[, rna_expression_fpkm := sequences$gene_FPKM]
+    } else {
+      normal = data.table()
     }
     
-    if (n_tumor>0){
-      peptide=sapply(seq(1,n_tumor,1),function(i) substr(x = sequences$peptidecontexttumor,start = i, stop = i+(peptidelength-1)))
-      c_term_pos=seq(peptidelength,nchar(sequences$peptidecontexttumor),1)
-      tumor=unique(x = data.table(peptide,c_term_pos), by = "peptide")
-      tumor[,variant_id := sequences$variant_id]
-      tumor[,gene_symbol := sequences$symbol]
-      tumor[,rna_expression_fpkm := sequences$gene_FPKM]
-    } else{
-      tumor=data.table()
+    if (n_tumor > 0) {
+      peptide = sapply(seq(from = 1, to = n_tumor, by = 1), function(i) substr(x = sequences$peptidecontexttumor,
+                                                                               start = i,
+                                                                               stop = i + (peptidelength - 1)))
+      c_term_pos = seq(from = peptidelength,
+                       to = nchar(sequences$peptidecontexttumor),
+                       by = 1)
+      tumor = unique(x = data.table(peptide, c_term_pos),
+                     by = "peptide")
+      tumor[, variant_id := sequences$variant_id]
+      tumor[, gene_symbol := sequences$symbol]
+      tumor[, rna_expression_fpkm := sequences$gene_FPKM]
+    } else {
+      tumor = data.table()
     }
     
     # select tumor peptides != normal peptides; select corresponding normal peptides
-    tumor=tumor[tumor$peptide %ni% normal$peptide]
-    normal=normal[match(tumor$c_term_pos,normal$c_term_pos,nomatch = FALSE)]
+    tumor = tumor[tumor$peptide %ni% normal$peptide]
+    normal = normal[match(x = tumor$c_term_pos, table = normal$c_term_pos, nomatch = FALSE)]
     
-    return(list(normal,tumor))
+    return(list(normal, tumor))
   }
 }
