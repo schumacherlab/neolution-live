b09f84e42f7f89fcb4dcb70c80335a17407ac8ef
Lorenzo Fanchi <l.fanchi@nki.nl>
2015-12-01 14:02:11 +0100

    comment & style
    Former-commit-id: 477441852b8209440ea835aa536ca7c428b0ead6

--- support_scripts/workflowPairedPredictions.R
+++ support_scripts/workflowPairedPredictions.R
@@ -37,15 +37,18 @@ performPairedSequencePredictions = function(file, allele, peptidelength, affcuto
                                    peptidelength = peptidelength)
     peptideStretchVector = c(variantInfo[i, ]$peptidecontextnormal, variantInfo[i, ]$peptidecontexttumor)
     
+    # vectors with colnames and colclasses for making empty tables when needed
     columnNamesEmptyTable = c(names(variantInfo)[-match(x = c("peptidecontextnormal", "peptidecontexttumor"), table = names(variantInfo))],
                               "c_term_pos", "hla_allele", "xmer",
                               "tumor_peptide", "tumor_c_term_aa", paste0("tumor_", allele, "affinity"), "tumor_processing_score",
                               "normal_peptide", "normal_c_term_aa", paste0("normal_", allele, "affinity"), "normal_processing_score")
+    
     columnClassesEmptyTable = c(unlist(lapply(variantInfo, class)[-match(x = c("peptidecontextnormal", "peptidecontexttumor"), table = names(variantInfo))],
                                        use.names=FALSE),
                                 "numeric", "character", "numeric",
                                 "character", "character", "numeric", "numeric",
                                 "character", "character", "numeric", "numeric")
+    
     # if no tumor peptides found, move to next line
     if (nrow(peptideList[[2]]) < 1) {
       mergedTumorPredictionsWithFiltersApplied = emptyTableWithColumnNamesAndColumnClasses(colnames = columnNamesEmptyTable,
