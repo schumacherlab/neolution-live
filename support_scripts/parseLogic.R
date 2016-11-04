834f5e7231068c76bf88fc1f24068639a4036dbd
Lorenzo Fanchi <l.fanchi@nki.nl>
2016-11-04 19:02:00 +0100

    exclude unnecessary columns
    Former-commit-id: 73f0ce58cf5d7878053aa9a9c40a2f22489bddfb

--- support_scripts/parseLogic.R
+++ support_scripts/parseLogic.R
@@ -50,7 +50,7 @@ processVariants = function(sid, variants) {
                new = c("peptidecontextnormal", "peptidecontexttumor"))
 
       variantssubset = unique(x = subset(x = variants,
-                                         select = names(variants) %ni% c("codon_ref", "aa_ref", "aa_pos_ref", "protein_seq_ref")),
+                                         select = names(variants) %ni% c("codon_ref", "aa_ref", "aa_pos_ref", "protein_seq_ref", 'ref_allele_count', 'alt_allele_count', 'vaf')),
                               by = c("peptidecontextnormal", "peptidecontexttumor"))
     } else if (all(c("Gene", "transcriptid", "peptidecontextnormal", "peptidecontexttumor", "Cufflinks FPKM value (expression level)") %in% names(variants))) {
       # dealing with Sanger data: rename column headers, take subset
