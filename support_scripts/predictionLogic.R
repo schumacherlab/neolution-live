a690fc6403f91f50ad5736788e70baa5579d6e94
Lorenzo Fanchi <l.fanchi@nki.nl>
2016-12-04 16:42:11 +0100

    fixed structural variants-specific issue in case of no peptides
    Former-commit-id: ff24aa55faad194e84625bacf661fce12631e0bc

--- support_scripts/predictionLogic.R
+++ support_scripts/predictionLogic.R
@@ -18,16 +18,20 @@ performParallelPredictions = function(peptides, peptidestretch, allele, peptidel
 
     # merge prediction info
     predictions = lapply(seq(1, length(affinityPredictions)),
-                         function(x) {
-                           data = merge(x = peptides[[x]],
-                                        y = unique(x = affinityPredictions[[x]],
-                                                   by = "peptide"),
-                                        by = "peptide")
-
-                           data = merge(x = data,
-                                        y = processingPredictions[[x]],
-                                        by = "c_term_pos")
-                         })
+    										 function(x) {
+    										 	if (nrow(peptides[[x]]) < 1) {
+    										 		return(data.table())
+    										 	}
+    										 	data = merge(x = peptides[[x]],
+    										 							 y = unique(x = affinityPredictions[[x]],
+    										 							 					 by = "peptide"),
+    										 							 by = "peptide")
+    										 	
+    										 	data = merge(x = data,
+    										 							 y = processingPredictions[[x]],
+    										 							 by = "c_term_pos")
+    										 	return(data)
+    										 })
 
     predictions = rbindlist(predictions)
 
@@ -66,94 +70,98 @@ performParallelPredictions = function(peptides, peptidestretch, allele, peptidel
 }
 
 performAffinityPredictions = function(peptides, allele, peptidelength) {
-  # generate random number to give to temp dir and temp file
-  randomNumber = ceiling(runif(n = 1,
-                               min = 0,
-                               max = 10 ^ 10))
-
-  dir.create(paste0(temporaryDirectoryPath, "/", randomNumber))
-
-  # write peptides to disk in temp dir
-  invisible(sapply(seq(1, length(peptides), 1), function(x)
-    write(x = sprintf("%s", peptides[x]),
-          file = paste0(temporaryDirectoryPath, "/", randomNumber, "/", randomNumber, "_peps.fas"),
-          append = TRUE,
-          sep = "\n")))
-
-  ## use on local Mac
-  # perform predictions on HPC from local Mac
-  # sshconn = pipe(paste0('ssh -l l.fanchi paranoid "',
-  #                    paste0("nice -n 9 ",
-  #                           predictorPaths$netMHCpan,
-  #                           ' -a HLA-',gsub('^([A-Z]{1}[0-9]{2})([0-9]{2})$', '\\1:\\2', allele),
-  #                           ' -l ',peptidelength,
-  #                           ' -f ',fastafile,'"'))) # start pipe to paranoid
-  # output = readLines(sshconn) # start job on paranoid and read terminal output
-  # close(sshconn) # close pipe
-
-  ## use on HPC
-  # perform predictions
-  command = ifelse(test = runParameters$panversion == "3.0",
-                   yes = paste0(# "nice -n 9 ",
-                     predictorPaths$netMHCpan,
-                     ' -a HLA-', gsub(pattern = '^([A-Z]{1}[0-9]{2})([0-9]{2})$',
-                                      replacement = '\\1:\\2',
-                                      x = allele),
-                     ' -l ', peptidelength,
-                     ' -p ',
-                     ' -f ', paste0(temporaryDirectoryPath, "/", randomNumber, "/", randomNumber, "_peps.fas")),
-                   no = paste0(# "nice -n 9 ",
-                     predictorPaths$netMHCpan,
-                     ' -a HLA-', gsub(pattern = '^([A-Z]{1}[0-9]{2})([0-9]{2})$',
-                                      replacement = '\\1:\\2',
-                                      x = allele),
-                     ' -l ', peptidelength,
-                     ' -p ',
-                     ' -f ', paste0(temporaryDirectoryPath, "/", randomNumber, "/", randomNumber, "_peps.fas"),
-                     ' -tdir ', paste0(temporaryDirectoryPath,"/",randomNumber),
-                     ' -ic50')
-  )
-  output = system(command = command,
-                  intern = TRUE)
-
-  file.remove(paste0(temporaryDirectoryPath, "/", randomNumber, "/", randomNumber, "_peps.fas"))
-  file.remove(paste0(temporaryDirectoryPath, "/", randomNumber))
-
-  # perform regex on netMHC output
-  processPreditionOutput = function(raw_output) {
-    raw_output = raw_output[-grep(pattern = "^\\#.+|^\\-.+|^Protein.+|pos.+|^HLA.+|^$",
-                                  ignore.case = TRUE,
-                                  x = raw_output)]
-    raw_output = gsub(pattern = "^[[:blank:]]+| <= WB| <= SB",
-                      replacement = "",
-                      x = raw_output)
-    clean_output = gsub(pattern = "[[:blank:]]+",
-                        replacement = "\t",
-                        x = raw_output)
-    if (length(clean_output) > 0 & runParameters$panversion == "3.0") {
-      data = as.data.table(read.table(text = clean_output,
-                                      stringsAsFactors = FALSE))
-      setnames(x = data, old = names(data), new = c("position", "hla_allele", "peptide", "peptide_core", "Of", "Gp", "Gl", "Ip", "Il", "Icore", "variant_id",
-                                                    "peptide_score_log50k", paste0(allele, "affinity"), paste0(allele, "percentile_rank")))
-    } else if (length(clean_output) > 0) {
-      data = as.data.table(read.table(text = clean_output,
-                                      stringsAsFactors = FALSE))
-      setnames(x = data, old = names(data), new = c("position", "hla_allele", "peptide", "variant_id",
-                                                    "peptide_score_log50k", paste0(allele, "affinity"), paste0(allele, "percentile_rank")))
-    } else {
-      data = emptyTableWithColumnNamesAndColumnClasses(colnames = c("position", "hla_allele", "peptide", "variant_id", "peptide_score_log50k", paste0(allele, "affinity"), paste0(allele, "percentile_rank")),
-                                                       colclasses = c("numeric", "character", "character", "numeric", "numeric", "numeric", "numeric"))
-    }
-    return(data)
-  }
-
-  data = processPreditionOutput(output)
+	if (length(peptides) < 1 ) {
+		data = processPredictionOutput('')
+	} else {
+		# generate random number to give to temp dir and temp file
+		randomNumber = ceiling(runif(n = 1,
+																 min = 0,
+																 max = 10 ^ 10))
+		
+		dir.create(paste0(temporaryDirectoryPath, "/", randomNumber))
+		
+		# write peptides to disk in temp dir
+		invisible(sapply(seq(1, length(peptides), 1), function(x)
+			write(x = sprintf("%s", peptides[x]),
+						file = paste0(temporaryDirectoryPath, "/", randomNumber, "/", randomNumber, "_peps.fas"),
+						append = TRUE,
+						sep = "\n")))
+		
+		## use on local Mac
+		# perform predictions on HPC from local Mac
+		# sshconn = pipe(paste0('ssh -l l.fanchi paranoid "',
+		#                    paste0("nice -n 9 ",
+		#                           predictorPaths$netMHCpan,
+		#                           ' -a HLA-',gsub('^([A-Z]{1}[0-9]{2})([0-9]{2})$', '\\1:\\2', allele),
+		#                           ' -l ',peptidelength,
+		#                           ' -f ',fastafile,'"'))) # start pipe to paranoid
+		# output = readLines(sshconn) # start job on paranoid and read terminal output
+		# close(sshconn) # close pipe
+		
+		## use on HPC
+		# perform predictions
+		command = ifelse(test = runParameters$panversion == "3.0",
+										 yes = paste0(# "nice -n 9 ",
+										 	predictorPaths$netMHCpan,
+										 	' -a HLA-', gsub(pattern = '^([A-Z]{1}[0-9]{2})([0-9]{2})$',
+										 									 replacement = '\\1:\\2',
+										 									 x = allele),
+										 	' -l ', peptidelength,
+										 	' -p ',
+										 	' -f ', paste0(temporaryDirectoryPath, "/", randomNumber, "/", randomNumber, "_peps.fas")),
+										 no = paste0(# "nice -n 9 ",
+										 	predictorPaths$netMHCpan,
+										 	' -a HLA-', gsub(pattern = '^([A-Z]{1}[0-9]{2})([0-9]{2})$',
+										 									 replacement = '\\1:\\2',
+										 									 x = allele),
+										 	' -l ', peptidelength,
+										 	' -p ',
+										 	' -f ', paste0(temporaryDirectoryPath, "/", randomNumber, "/", randomNumber, "_peps.fas"),
+										 	' -tdir ', paste0(temporaryDirectoryPath,"/",randomNumber),
+										 	' -ic50')
+		)
+		output = system(command = command,
+										intern = TRUE)
+		
+		file.remove(paste0(temporaryDirectoryPath, "/", randomNumber, "/", randomNumber, "_peps.fas"))
+		file.remove(paste0(temporaryDirectoryPath, "/", randomNumber))
+		
+		data = processPredictionOutput(output)
+	}
 
   data = subset(x = data,
                 select = colnames(data[, -dropNa(match(x = c("position", "variant_id", "peptide_score_log50k", "peptide_core", "Of", "Gp", "Gl", "Ip", "Il", "Icore"),
                                                         table = names(data))),
                                        with = FALSE]))
   return(data)
+}
+
+# perform regex on netMHC output
+processPredictionOutput = function(raw_output) {
+	raw_output = raw_output[-grep(pattern = "^\\#.+|^\\-.+|^Protein.+|pos.+|^HLA.+|^$",
+																ignore.case = TRUE,
+																x = raw_output)]
+	raw_output = gsub(pattern = "^[[:blank:]]+| <= WB| <= SB",
+										replacement = "",
+										x = raw_output)
+	clean_output = gsub(pattern = "[[:blank:]]+",
+											replacement = "\t",
+											x = raw_output)
+	if (length(clean_output) > 0 & runParameters$panversion == "3.0") {
+		data = as.data.table(read.table(text = clean_output,
+																		stringsAsFactors = FALSE))
+		setnames(x = data, old = names(data), new = c("position", "hla_allele", "peptide", "peptide_core", "Of", "Gp", "Gl", "Ip", "Il", "Icore", "variant_id",
+																									"peptide_score_log50k", paste0(allele, "affinity"), paste0(allele, "percentile_rank")))
+	} else if (length(clean_output) > 0) {
+		data = as.data.table(read.table(text = clean_output,
+																		stringsAsFactors = FALSE))
+		setnames(x = data, old = names(data), new = c("position", "hla_allele", "peptide", "variant_id",
+																									"peptide_score_log50k", paste0(allele, "affinity"), paste0(allele, "percentile_rank")))
+	} else {
+		data = emptyTableWithColumnNamesAndColumnClasses(colnames = c("position", "hla_allele", "peptide", "variant_id", "peptide_score_log50k", paste0(allele, "affinity"), paste0(allele, "percentile_rank")),
+																										 colclasses = c("numeric", "character", "character", "numeric", "numeric", "numeric", "numeric"))
+	}
+	return(data)
 }
 
 performProcessingPredictions = function(peptidestretch) {
