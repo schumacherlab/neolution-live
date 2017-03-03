e212e99416501105ca36421d721a79d3c68c97db
Lorenzo Fanchi <l.fanchi@nki.nl>
2017-03-03 14:37:27 +0100

    make options less cluttered
    Former-commit-id: e02ae38947947afb2c481530eb15982e04edd0ee

--- support_scripts/runConfig.R
+++ support_scripts/runConfig.R
@@ -1,14 +1,11 @@
 #### Lookup parameters and other configuration options can be set here ####
 
-# set number of cores available for parallel processing
-numberOfWorkers = detectCores()/4
+runOptions = list(general = list(numberOfWorkers = detectCores()/4, # set number of cores available for parallel processing
+																 temporaryDirectoryPath = '~/scratch'), # set path of temporary directory (set chmod 1777 on tempdir)
 
-# set predictor paths
-predictorPaths = data.table(netMHCpan = paste0("/home/NFS/users/l.fanchi/predictors/netMHCpan-", runParameters$panversion, "/netMHCpan"),
-                            netChop = "/home/NFS/users/l.fanchi/predictors/netchop-3.1/bin/netChop")
+									predictors = list(netMHCpan = paste0('/home/NFS/users/l.fanchi/predictors/netMHCpan-', runParameters$panversion, '/netMHCpan'),
+																		netChop = '/home/NFS/users/l.fanchi/predictors/netchop-3.1/bin/netChop'),
 
-# set path of self-epitope lists
-selfEpitopeListPath = "./selflists"
-
-# set path of temporary directory (set chmod 1777 on tempdir)
-temporaryDirectoryPath = "~/scratch"
+									resources = list(selfEpitopeListPath = './selflists', # set path of self-epitope lists
+																	 randomForestModelPath = './resources/rf_model.RData') # set path to random forest model
+									)
