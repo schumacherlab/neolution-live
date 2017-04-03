2eaceab9e6475f294eee1812c60bd45d77b76756
Lorenzo Fanchi <l.fanchi@nki.nl>
2017-04-03 16:10:55 +0200

    add list of regex patterns
    Former-commit-id: db30035376a610447c75d006005fafe9fe9ae322
    Former-commit-id: f0d59dc28bbc7360150eec01b570af9a20ca32e5

--- support_scripts/runConfig.R
+++ support_scripts/runConfig.R
@@ -9,3 +9,11 @@ runOptions = list(general = list(numberOfWorkers = detectCores()/4, # set number
 									resources = list(selfEpitopeListPath = './selflists', # set path of self-epitope lists
 																	 randomForestModelPath = './resources/rf_model.RData') # set path to random forest model
 									)
+
+regexPatterns = list(file_extension = '\\.[^.]+$', # match file extension (everything after last dot, inclusive)
+										 snp_identifier = '[gr]s\\d+', # for matching SNPs
+										 gs_identifier = 'gs\\d+', # for matching snps not found in dbSNP, keep boundless (no '^' or '$')
+										 rs_identifier = 'rs\\d+', # for matching snps found in dbSNP, keep boundless (no '^' or '$')
+										 cosmic_identifier = 'COSM\\d+', # for matching variants found in COSMIC coding muts database, keep boundless
+										 seqdata_prefix = '_mg.+|_S\\d_L.+|-ra-bq.+', # for isolating GCF prefix
+										 allele_exclusion = 'C[0-9]{4}') # for excluding particular alleles from analysis
