aba430b0a94fabc67d8f89546e63cd9b2d6dd47a
Lorenzo Fanchi <l.fanchi@nki.nl>
2017-08-17 19:18:58 +0200

    simplified path configuration
    Former-commit-id: 2ed1f82e2af6e700512bc78a1b8d7f85b80902d8
    Former-commit-id: 9af22a16abc292a72165d0182784db4a8146ead1

--- support_scripts/runConfig.R
+++ support_scripts/runConfig.R
@@ -1,10 +1,11 @@
 #### Lookup parameters and other configuration options can be set here ####
+data_path = '/DATA/users/l.fanchi'
 
 runOptions = list(general = list(numberOfWorkers = detectCores()/4, # set number of cores available for parallel processing
-																 temporaryDirectoryPath = '~/scratch'), # set path of temporary directory (set chmod 1777 on tempdir)
+																 temporaryDirectoryPath = file.path(data_path, 'scratch')), # set path of temporary directory (set chmod 1777 on tempdir)
 
-									predictors = list(netMHCpan = paste0('/home/NFS/users/l.fanchi/predictors/netMHCpan-', runParameters$panversion, '/netMHCpan'),
-																		netChop = '/home/NFS/users/l.fanchi/predictors/netchop-3.1/bin/netChop'),
+									predictors = list(netMHCpan = file.path(data_path, paste0('predictors/netMHCpan-', runParameters$panversion, '/netMHCpan')),
+																		netChop = file.path(data_path, 'predictors/netchop-3.1/bin/netChop')),
 
 									resources = list(selfEpitopeListPath = './selflists', # set path of self-epitope lists
 																	 randomForestModelPath = './resources/rf_model.RData') # set path to random forest model
