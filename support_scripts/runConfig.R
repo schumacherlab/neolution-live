863c87fb0d45642117dd969301182dd1c6ceb3f9
Lorenzo Fanchi <l.fanchi@nki.nl>
2017-12-18 14:58:21 +0100

    update paths

--- support_scripts/runConfig.R
+++ support_scripts/runConfig.R
@@ -1,11 +1,11 @@
 #### Lookup parameters and other configuration options can be set here ####
-data_path = '/DATA/users/l.fanchi'
+data_path = '/DATA'
 
 runOptions = list(general = list(numberOfWorkers = round(detectCores()/6, 0), # set number of cores available for parallel processing
-                                 temporaryDirectoryPath = file.path(data_path, 'scratch')), # set path of temporary directory (set chmod 1777 on tempdir)
+                                 temporaryDirectoryPath = file.path(data_path, 'NetMHCtmp')), # set path of temporary directory (set chmod 1777 on tempdir)
 
-                  predictors = list(netMHCpan = file.path(data_path, paste0('predictors/netMHCpan-', runParameters$panversion, '/netMHCpan')),
-                                    netChop = file.path(data_path, 'predictors/netchop-3.1/bin/netChop')),
+                  predictors = list(netMHCpan = file.path(data_path, paste0('resources/predictors/netMHCpan-', runParameters$panversion, '/netMHCpan')),
+                                    netChop = file.path(data_path, 'resources/predictors/netchop-3.1/bin/netChop')),
 
                   resources = list(selfEpitopeListPath = file.path(data_path, 'resources/neolution_selflists'), # set path of self-epitope lists
                                    randomForestModelPath = './resources/rf_model.RData') # set path to random forest model
